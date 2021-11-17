6f88ef779a8a9e51f4edd3467c3b4d86e32f8af1
Rob Taylor <roboncode@gmail.com>
2021-11-17 07:26:20 -0700

    removed unused files

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,7 +2,6 @@
   <main class="vuwi-main">
     <div class="vuwi-container navbar responsive">
       <router-view />
-      <Footer />
       <div class="mt-5 mx-auto text-center opacity-25 text-sm">
         <span>[Default Layout]</span>
       </div>
