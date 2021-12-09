edd1b0c3982120b47faec9e1f10a13b3da1dc923
Rob Taylor <roboncode@gmail.com>
2021-12-09 10:33:56 -0700

    removed all dev deps; adding vuwiClass to components

---
+++ components/Avatar/Avatar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="vuwiClass">
+    <img v-if="photo" :src="photo" :alt="name" />
+    <div v-else-if="name" class="initial">
+      {{ initial }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    vuwiClass: {
+      type: String,
+      default: 'vuwi-avatar',
+    },
+    photo: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props) {
+    const initial = computed(() => {
+      if (props.name)
+        return props.name[0]
+    })
+
+    return {
+      initial,
+    }
+  },
+})
+</script>
---
+++ components/Dialog/Dialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="vuwiClass" class="vuwi-light-dark">
+    <div class="p-2 flex items-center ">
+      <div class="flex-grow">
+        <slot name="title"></slot>
+      </div>
+      <button
+        class="vuwi-btn vuwi-btn-icon"
+        @click="$emit('close')"
+      >
+        <tabler-x class="h-6 w-6" />
+      </button>
+    </div>
+    <Line />
+    <slot></slot>
+    <slot name="actions"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    vuwiClass: {
+      type: String,
+      default: 'vuwi-dialog',
+    },
+  },
+  emits: ['close'],
+})
+</script>
