1bdfd13795e7339833c18492f4de263b23bb191d
Rob Taylor <roboncode@gmail.com>
2021-12-22 08:34:16 -0700

    added progress bar

---
+++ components/Progress/VuwiProgressBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps({
+  percent: {
+    type: Number,
+    default: 0,
+  },
+})
+
+const meterStyle = computed(() => {
+  return {
+    width: `${props.percent}%`,
+  }
+})
+</script>
+
+<template>
+  <div>
+    <div class="relative" :style="meterStyle">
+      <slot></slot>
+    </div>
+  </div>
+</template>
