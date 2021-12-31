b988c6cce346834e69f348e66d0d8e57fa604077
Rob Taylor <roboncode@gmail.com>
2021-12-31 15:44:20 -0700

    adding vue alert,button components; updated styles

---
+++ components/Button/VuwiButton.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default defineComponent({
+  props: {
+    rootClass: {
+      type: String,
+      default: 'vuwi-btn',
+    },
+  },
+})
+</script>
+
+<template>
+  <button :class="rootClass">
+    <slot></slot>
+  </button>
+</template>
