f9ecdb5aabbfdbdd27daf8037d1606f92d9bf095
Rob Taylor <roboncode@gmail.com>
2021-12-31 17:15:12 -0700

    moved vuwi css styling into single file

---
+++ components/Spinner/VuwiSpinner.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default defineComponent({
+  props: {
+    rootClass: {
+      type: String,
+      default: 'vuwi-spinner',
+    },
+  },
+})
+</script>
+
+<template>
+  <div :class="rootClass">
+    <slot></slot>
+  </div>
+</template>
---
+++ components/Window/VuwiWindow.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default defineComponent({
+  props: {
+    rootClass: {
+      type: String,
+      default: 'vuwi-window',
+    },
+  },
+})
+</script>
+
+<template>
+  <div :class="rootClass">
+    <div :class="`${rootClass}-content`">
+      <slot></slot>
+    </div>
+  </div>
+</template>
