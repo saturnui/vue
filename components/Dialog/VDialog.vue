c850c93dd6d96b23067a9a4e952b92008e2769a5
Rob Taylor <roboncode@gmail.com>
2022-01-03 08:20:43 -0700

    updated dialog name and style

---
+++ components/Dialog/VDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wi-dialog">
+    <div class="wi-dialog-titlebar">
+      <slot name="title" />
+      <div class="flex-grow"></div>
+      <VButton class="wi-btn wi-btn-icon" @click="$emit('close')">
+        <tabler-x class="wi-dialog-close-icon" />
+      </VButton>
+    </div>
+    <slot />
+    <div class="wi-dialog-actions">
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    wi: {
+      type: String,
+      default: 'wi-dialog',
+    },
+  },
+  emits: ['close'],
+})
+</script>
